4844784bfc7cbcb16e606692dbc052375917c51f
Kanyisa-Ntombini <kanyisa.ntombini@gmail.com>
2021-07-20 09:46:05 +0200

    removed hello world file

--- greet_xola.js
+++ greet_xola.js
@@ -1,6 +1,16 @@
 const chalk = require('chalk');
 const greet = require('./greet');
+const figlet = require('figlet');
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
 
 console.log(styledMessage);
+
+figlet('Molo Mamgcina!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
